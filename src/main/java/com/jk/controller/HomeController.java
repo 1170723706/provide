da78a6342d4144d40b93a258c365c1a87e2b7e69
xjw163com <1632622685@qq.com>
2019-03-05 21:08:14 +0800

    aa

--- src/main/java/com/jk/controller/HomeController.java
+++ src/main/java/com/jk/controller/HomeController.java
@@ -1,36 +1,14 @@
 package com.jk.controller;
 
-import com.jk.bean.Exprent;
-import com.jk.service.HomeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("home")
 public class HomeController {
 
-    @Autowired
-    HomeService homeService;
-
     @RequestMapping("ToPage")
     public String ToPage(String name){
         return name;
     }
-
-    @ResponseBody
-    @RequestMapping("getzhuanjia")
-    public List<Exprent> getzhuanjia(){
-        List<Exprent> list = homeService.getzhuanjia();
-        return list;
-    }
-    @ResponseBody
-    @RequestMapping("getzanmei")
-    public List<Exprent> getzanmei(){
-        return homeService.getzanmei();
-    }
-
 }

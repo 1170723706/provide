2d6343d11956fba95b34d75f4936305af9f47283
XZH <1170723706@qq.com>
2019-03-05 18:32:17 +0800

    前台页面

--- src/main/java/com/jk/controller/HomeController.java
+++ src/main/java/com/jk/controller/HomeController.java
@@ -1,4 +1,14 @@
 package com.jk.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("home")
 public class HomeController {
+
+    @RequestMapping("ToPage")
+    public String ToPage(String name){
+        return name;
+    }
 }

b50b1ab30377f7c095041bcdfbd4837c87293f93
root <1075118340@qq.com>
2019-03-07 18:43:25 +0800

    方法

--- src/main/java/com/jk/controller/BckController.java
+++ src/main/java/com/jk/controller/BckController.java
@@ -1,7 +1,12 @@
 package com.jk.controller;
 
 import com.jk.bean.Example;
+import com.jk.bean.PingLun;
 import com.jk.service.BckService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +20,27 @@ public class BckController {
 
     @Resource
     BckService bckService;
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @ResponseBody
+    @RequestMapping("addPingLun")
+    public void addPingLun(PingLun pingLun){
+
+        mongoTemplate.save(pingLun);
+    }
+
+    @ResponseBody
+    @RequestMapping("queryPingLun")
+    public List<PingLun> queryPingLun(Integer id){
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("em_id").is(id));
+        List<PingLun> pingLuns = mongoTemplate.find(query, PingLun.class);
+        return pingLuns;
+    }
+
 
     @ResponseBody
     @RequestMapping("queryExample1")

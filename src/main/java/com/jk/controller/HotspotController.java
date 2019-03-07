a0d94fb7c6e93c4e980d31f9e4454ea5f0f9d965
windFlde <wind13934954475@163.com>
2019-03-08 07:56:49 +0800

    .....

---
+++ src/main/java/com/jk/controller/HotspotController.java
@@ -0,0 +1,39 @@
+package com.jk.controller;
+
+import com.jk.bean.Hotspot;
+import com.jk.service.HotspotService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("rdzx")
+public class HotspotController {
+
+    @Resource
+    private HotspotService hotspotService;
+
+    @RequestMapping("queryHotspot")
+    public List<Hotspot> queryHotspot(Integer page, Integer rows) {
+
+        List<Hotspot> list=hotspotService.queryHotspot(page,rows);
+        return list;
+    }
+
+    @RequestMapping("queryHotspotChildren")
+    public Hotspot queryHotspotChildren(Hotspot hotspot) {
+
+        Hotspot hotspots=hotspotService.queryHotspotChildren(hotspot);
+        return hotspots;
+    }
+
+    @RequestMapping("updateRdzxOne")
+    public void updateRdzxOne(Integer id) {
+
+        hotspotService.updateRdzxOne(id);
+
+    }
+
+}
